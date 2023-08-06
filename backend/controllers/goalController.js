e1641c18bd4503e3103a2032172386b72819df25
palak <palakhandelwalgp@gmail.com>
2023-08-06 16:29:49 +0530

    added models

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,36 +1,59 @@
+const asyncHandler=require('express-async-handler');
+
+const Goal = require('../model/goalModel.js')
+
+
 //@desc Get goals
 //@route  GET/api/goals
 //@access private
-const getGoals = (req, res) => {
-  res.status(200).send({ message: "get goals" });
-};
+const getGoals = asyncHandler(async(req, res) => {
+  const goals=await Goal.find()
+  res.status(200).send(goals);
+});
 
 //@desc Set goals
 //@route  POST/api/goals
 //@access private
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async(req, res) => {
   if(!req.body.text)
   {
     // res.status(400).send({ message: "please add a text field" })
     res.status(400)
     throw new Error('please add a text field')
   }
-  res.status(200).send({ message: "set goals" });
-};
+  const goal=await Goal.create({
+    text : req.body.text
+  })
+  res.status(200).send(goal);
+});
 
 //@desc update goals
 //@route  PUT/api/goals/:id
 //@access private
-const updateGoal = (req, res) => {
-  res.status(200).send({ message: `update goal ${req.params.id}` });
-};
+const updateGoal = asyncHandler(async(req, res) => {
+
+ const goal=await Goal.findById(req.params.id)
+ if(!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+ }
+  const updatedGoal=await Goal.findByIdAndUpdate(req.params.id,req.body,
+    {new:true})
+  res.status(200).send(updatedGoal);
+});
 
 //@desc delete goals
 //@route  DELETE/api/goals/:id
 //@access private
-const deleteGoal = (req, res) => {
-  res.status(200).send({ message: `delete goal ${req.params.id}` });
-};
+const deleteGoal = asyncHandler(async(req, res) => {
+    const goal=await Goal.findById(req.params.id)
+    if(!goal){
+       res.status(400)
+       throw new Error('Goal not found')
+    }
+    await goal.deleteOne()
+  res.status(200).send({ id: req.params.id});
+});
 
 module.exports = {
   getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express=require('express');
 const dotenv=require('dotenv');
 const router=require('./routes/goalRoutes.js')
+const {errorHandler} =require('./middleware/errorMiddleware.js')
+const {connectDB}=require('./config/db.js')
+
+
+connectDB();
 
 const app=express();
 const port=process.env.PORT || 5000;
@@ -10,6 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use('/api/goals',router)
+app.use(errorHandler)
 
 app.listen(port,()=>{
     console.log(`server started at port ${port}`)
